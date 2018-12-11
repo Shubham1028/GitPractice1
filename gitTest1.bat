4835dda437c75013f26e0fa6d95c067c9f73fdec
Shubham1028 <SDwivedi2@ad.harman.com>
2018-12-11 14:32:10 +0530

    Added index.html and modified first test file

--- gitTest1.bat
+++ gitTest1.bat
@@ -1,10 +1,3 @@
- Volume in drive D is DATA
- Volume Serial Number is AA07-09E6
-
- Directory of D:\Practice\myProject
-
-12/11/2018  01:52 PM    <DIR>          .
-12/11/2018  01:52 PM    <DIR>          ..
-12/11/2018  01:52 PM                 0 gitTest1.bat
-               1 File(s)              0 bytes
-               2 Dir(s)  334,402,633,728 bytes free
+ECHO OFF
+ECHO First GIT testing
+PAUSE
